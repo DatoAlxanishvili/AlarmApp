5a4e586467bfc40cbb12e0031b9f6ce180cb50c7
Davit Alkhanishvili <davit.alkhanishvili02@geolab.edu.ge>
2015-12-13 00:00:52 +0400

    Assignment done

--- AlarmApp/app/src/main/java/assign4/alarmapp/MainActivity.java
+++ AlarmApp/app/src/main/java/assign4/alarmapp/MainActivity.java
@@ -26,38 +26,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-       activity=this;
+        activity = this;
 
 
-
-     /*   Button clickAlarm = (Button) findViewById(R.id.button);
-        clickAlarm.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Calendar c = Calendar.getInstance();
-                c.add(Calendar.MINUTE, 2);
-                long date = c.getTimeInMillis();
-
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.setAction(Intent.ACTION_MAIN);
-
-                PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 1, intent, 0);
-
-                AlarmManager alarmManager = (AlarmManager) activity.getSystemService(Context.ALARM_SERVICE);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, date, pendingIntent);
-            }
-        });*/
-
-
-
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     public void onClick(View v) {
